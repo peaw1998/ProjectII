85b1731a2d08dd03b44a9b9ba17d949a04869a83
twoherohon1 <twoherohon1@hotmail.com>
2020-04-06 00:38:10 +0700

    Login Form

--- scr/Login/LoginScreen.js
+++ scr/Login/LoginScreen.js
@@ -32,25 +32,41 @@ export default class LoginScreen extends Component {
           />
         </View>
         <Text style={styles.font}>LOG IN</Text>
-        <View style={styles.form}>
-          <TextInput style={{padding: 15}} placeholder="Username" />
+        <View style={styles.type}>
+          <Button style={styles.button3}>
+            <Text>Learner</Text>
+          </Button>
+          <Button style={styles.button3}>
+            <Text>Teacher</Text>
+          </Button>
         </View>
         <View style={styles.form}>
           <TextInput
-            style={{padding: 15}}
+            style={{color: '#fff'}}
+            placeholder="Username"
+            placeholderTextColor="#fff"
+          />
+        </View>
+        <View style={styles.form}>
+          <TextInput
+            style={{color: '#fff'}}
             placeholder="Password"
+            placeholderTextColor="#fff"
             secureTextEntry={true}
           />
         </View>
-        <Button>
-          <Text>Log in</Text>
-        </Button>
         <View>
-          <Button>
+          <Button style={styles.button}>
+            <Text>Log in</Text>
+          </Button>
+        </View>
+        <View style={styles.type}>
+          <Button style={styles.button3}>
             <Text>Sign up</Text>
           </Button>
-
-          <Text>forget Password</Text>
+          <Button style={styles.button2}>
+            <Text>Forget Password</Text>
+          </Button>
         </View>
       </View>
     );
@@ -64,20 +80,56 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#ede7f6',
+    backgroundColor: '#004163',
   },
   font: {
     fontSize: 35,
     fontFamily: 'Kanit-Bold',
-    color: '#217983',
+    color: '#fff',
+  },
+  font2: {
+    fontSize: 15,
+    fontFamily: 'Kanit-Bold',
+    color: '#fff',
+  },
+  button: {
+    width: 100,
+    justifyContent: 'center',
+    borderRadius: 30,
+    margin: 5,
+    backgroundColor: '#c0ffe6',
+  },
+  button3: {
+    width: 100,
+    justifyContent: 'center',
+    borderRadius: 30,
+    margin: 5,
+    backgroundColor: '#f5fffd',
+  },
+  button2: {
+    width: 140,
+    justifyContent: 'center',
+    borderRadius: 30,
+    margin: 5,
+    backgroundColor: '#f5fffd',
   },
   form: {
     margin: 10,
-    backgroundColor: '#e0f7fa',
+    backgroundColor: '#01579b',
     borderRadius: 30,
     height: 55,
     width: 300,
-    borderColor: '#81b9bf',
+    borderColor: '#f5fffd',
     borderWidth: 3,
+    alignItems: 'center',
+  },
+  type: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    // backgroundColor: '#b23751',
+    borderRadius: 40,
+    padding: 15,
   },
 });
